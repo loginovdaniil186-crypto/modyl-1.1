9a80519eefa7b8f6342ae2d60d3e8f003d7877c0
loginovdaniil186-crypto <loginovdaniil186@gmail.com>
2025-09-29 14:29:15 +0300

    Add files via upload

---
+++ Задание 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("введите число: ");
+            a = Console.Read();
+
+            if (a % 2 == 1)
+            {
+                Console.WriteLine("число нечетное");
+
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
---
+++ Задание 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем у пользователя радиус
+        Console.Write("Введите радиус круга: ");
+        double R = Convert.ToDouble(Console.ReadLine());
+
+        // Проверяем, что радиус не отрицательный
+        if (R < 0)
+        {
+            Console.WriteLine("Ошибка: радиус не может быть отрицательным. Пожалуйста, введите положительное значение.");
+        }
+        else
+        {
+            // Вычисляем площадь круга
+            double S = Math.PI * Math.Pow(R, 2);
+
+            // Выводим результат
+            Console.WriteLine($"Площадь круга с радиусом {R} равна {S:F2}");
+        }
+
+        Console.ReadKey();
+    }
+}
+
